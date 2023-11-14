8694c14c756769af7834881532ff2d45550bbad4
AysuDemirci <aysu.gunerr@gmail.com>
2023-11-14 21:27:17 +0300

    searchbox yapıldı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-import Navbar from './Components/Navbar';
-import './Style.css';
+import Navbar from "./Components/Navbar";
+import "./Style.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
     </div>
   );
 }
